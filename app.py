e24e14f944f1fc2f3950cf9c2d81f8672296866e
hassanahamed34 <hassanahamed136@gmail.com>
2026-05-16 22:25:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import streamlit as st
 
 st.title("Welcome to Streamlit")
 
+st.header("Section 1")
 st.write("Hello, Streamlit!")
 st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
 
 st.write(["list1", "list2"])
+
+st.header("Section 2")
 st.write("**Bold Text** and *Italic Text*")
