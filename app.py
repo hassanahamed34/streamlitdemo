6386952ac85fd392ad1932cc364047376a540f63
hassanahamed34 <hassanahamed136@gmail.com>
2026-05-17 22:46:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
 import streamlit as st
 
-st.title("Welcome to Streamlit")
+st.title("Welcome to Streamlit!")
 
-st.header("Section 1")
 st.write("Hello, Streamlit!")
 st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
-
-st.write(["list1", "list2"])
-
-st.header("Section 2")
-st.write("**Bold Text** and *Italic Text*")
-age = st.number_input("Enter your age:",
-                      min_value=0,
-                      max_value=120,
-                      value=25)
-st.write(f"Your age is {age}")
