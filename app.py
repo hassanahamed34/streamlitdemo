773205d8a8581030fa442dde8d9071ce8450885b
hassanahamed34 <hassanahamed136@gmail.com>
2026-05-17 23:15:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.write("Please enter the monethly sales target and select the region.")
 
 sales = st.number_input("Enter Monthly Sales Target (in USD):",
                         min_value = 0,
-                        max_value = 0,
+                        max_value = 50000,
                         value=50000)
 region = st.selectbox("Select Region:",
                      ["North", "South", "East", "West"])
