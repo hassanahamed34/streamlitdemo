bfe00e766e4b9dbc0e3e638b3b79168920b9df81
hassanahamed34 <hassanahamed136@gmail.com>
2026-05-17 23:37:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,12 +35,13 @@ operation_sci = st.selectbox("Choose scientific operation", ["Square Root",
                                                              "Tan"])
 
 value = st.number_input("Enter value", value=0.0)
-
+if operation_sci = "Power":
+    power = st.number_input("Enter power (if applicable)", value=2.0)
+    
 if st.button("Calculate Scientific"):
     if operation_sci == "Square Root":
         result = math.sqrt(value)
     elif operation_sci == "Power":
-        power = st.number_input("Enter power (if applicable)", value=2.0)
         result = math.pow(value, power)
     elif operation_sci == "Sin":
         result = math.sin(math.radians(value))
