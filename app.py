6a7ac277ed139c9db911adcc2d62fd731e14328d
hassanahamed34 <hassanahamed136@gmail.com>
2026-05-17 23:12:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ st.title("Retail Business Dashboard")
 
 st.header("Manager Input Section")
 
-st.write("Please provide your details below:")
+st.write("Please enter the monethly sales target and select the region.")
 
-age = st.number_input("Enter your age:",
-                      min_value=0,
-                      max_value=120,
-                      value=25)
-color = st.selectbox("Choose your favorite color:",
-                     ["Red", "Blue", "Green"])
+sales = st.number_input("Enter Monthly Sales Target (in USD):",
+                        min_value = 0,
+                        max_value = 0,
+                        value=50000)
+region = st.selectbox("Select Region:",
+                     ["North", "South", "East", "West"])
 
 if st.button("Submit"):
-    st.success(f"Thank you! Age: {age}, Favorite Color: {color}")
+    st.success(f"""The sales target entered: {sales} USD.
+                   The selected region: {region}""")
+  if sales > 100000:
+    st.write("Great! You have set an ambitious target!")
