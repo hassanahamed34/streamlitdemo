e89f866e3fb5f30c54a9b2a7f571b4ce95933310
hassanahamed34 <hassanahamed136@gmail.com>
2026-05-17 23:20:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ sales = st.number_input("Enter Monthly Sales Target (in USD):",
                         min_value = 0,
                         max_value = 50000,
                         value=50000)
+st.write("The sales target entered: {sales} USD.")
+
 region = st.selectbox("Select Region:",
                      ["North", "South", "East", "West"])
+st.write("The selected region: {region}")
 
 if st.button("Submit"):
-    st.success(f"""The sales target entered: {sales} USD.
-                   The selected region: {region}""")
+    st.success(f"Dashboard updated successfully!")
 
